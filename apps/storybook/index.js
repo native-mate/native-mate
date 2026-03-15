94a6eaa6f541c3aab00f9543c70951f0710309f4
arnavraj-7 <arnavrajcodes@gmail.com>
2026-03-15 22:59:58 +0530

    chore: fix monorepo workspace config and storybook setup
    - Update workspace config to exclude demo app from hoisting
    - Remove broken expo-router and reanimated plugins from storybook app.json
    - Fix storybook entry point and add .storybook/index.js
    - Align typescript version across workspace

--- apps/storybook/index.js
+++ apps/storybook/index.js
@@ -1,19 +1,4 @@
 import { registerRootComponent } from 'expo'
-import { AppRegistry } from 'react-native'
+import App from './src/App'
 
-const STORYBOOK_ENABLED = process.env.STORYBOOK_ENABLED === 'true'
-
-async function main() {
-  let App
-
-  if (STORYBOOK_ENABLED) {
-    const sb = await import('./.storybook')
-    App = sb.default
-  } else {
-    App = (await import('./src/App')).default
-  }
-
-  registerRootComponent(App)
-}
-
-main()
+registerRootComponent(App)
